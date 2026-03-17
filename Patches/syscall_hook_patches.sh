e3e4fae1d894b69844d3dd75221faf12bd11a2d0
JackA1ltman <cs2dtzq@163.com>
2026-03-17 12:36:30 +0800

    fix: some issues.
    Signed-off-by: JackA1ltman <cs2dtzq@163.com>

--- Patches/syscall_hook_patches.sh
+++ Patches/syscall_hook_patches.sh
@@ -108,7 +108,7 @@ for i in "${patch_files[@]}"; do
         if grep -q "ksu_handle_newfstat_ret" "drivers/kernelsu/ksud.c" >/dev/null 2>&1; then
             sed -i '/#if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)/i\#ifdef CONFIG_KSU\nextern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);\n#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)\nextern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);\n#endif\n#endif\n' fs/stat.c
             sed -i '/extern void ksu_handle_newfstat_ret/i\__attribute__((hot))\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user,\n\t\t\t\tint *flags);\n' fs/stat.c
-        elif "vfs_fstat" "fs/stat.c"; then
+        elif grep -q "vfs_fstat" "fs/stat.c" >/dev/null 2>&1; then
             sed -i '/EXPORT_SYMBOL(vfs_statx_fd);/a\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif\n' fs/stat.c
         else
             sed -i '/EXPORT_SYMBOL(vfs_fstat);/a\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif\n' fs/stat.c
